d86732b15050b7e8ea5e5241c6a26dd0fce3392a
jananiroma <janani.rmac@gmail.com>
2017-10-27 17:51:24 -0500

    changes

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-
 function initMap() {
-	var uluru = {lat: -25.363, lng: 131.044};
+	var uluru = {lat: 20.688715, lng: -101.356684};
 	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 4,
-		center: uluru
+		center: uluru,
+		zoom: 11
 	});
-	var marker = new google.maps.Marker({
-		position: uluru,
-		map: map
+	var layer = new google.maps.FusionTablesLayer({
+		query: {
+			select: 'SECCION',
+			from: '1if0DAMfJ6S-B2PJrP8zqwiyfOmr03-RHg2_iucTU'
+		}, 
+		styles: [{
+			polygonOptions: {
+				fillColor: '#89FC00',
+				fillOpactity: 0.3
+			}
+		}, {
+			where: 'SECCION = 1126',
+			polygonOptions: {
+				fillColor: '#5BC0EB'
+			}
+		}, {
+			where: 'SECCION = 1153',
+			polygonOptions: {
+				fillColor: '#FDE74C'
+			}
+		}, {
+			where: 'SECCION = 1168',
+			polygonOptions: {
+				fillColor: '#9BC53D'
+			}
+		}, {
+			where: 'SECCION = 1172',
+			polygonOptions: {
+				fillColor: '#FA7921'
+			}
+		}]
 	});
+	layer.setMap(map);
 }
